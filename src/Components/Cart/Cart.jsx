adf9bbfc272b3cd67813adda1fec09ae822abab2
IJE2704 <mh1669101@gmail.com>
2023-08-16 21:28:56 +0600

    total value add, tax add , shipping add, grand total value add

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,20 +6,25 @@ import './Cart.css'
 const Cart = (props) => {
     const cart = props.cart;
     let total =0;
+    let totalShipping =0
+    console.log(cart);
     for(const product of cart)
     {
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
     // console.log(total)
     return (
         <div className='cart'>
             <h5 className='order-summary'>Order Summary</h5>
             <div className='o-s-info'>
                 <p className='o-s-d'>Selected item : {cart.length}</p>
-                <p className='o-s-d'>Total price : ${total}</p>
-                <p className='o-s-d'>Total shiping Charge : $</p>
-                <p className='o-s-d'>Total tax : $</p>
-                <h6 className='g-t'>Grand total : $</h6>
+                <p className='o-s-d'>Total price : ${total.toFixed(2)}</p>
+                <p className='o-s-d'>Total shipping Charge : ${totalShipping}</p>
+                <p className='o-s-d'>Total tax : ${tax.toFixed(2)}</p>
+                <h6 className='g-t'>Grand total : ${grandTotal.toFixed(2)}</h6>
             </div>
             <div className='btn-container'>
                 <button className='btn-clear-cart'>
