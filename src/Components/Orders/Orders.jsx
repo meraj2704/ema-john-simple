80805623adecfb43ce449924c07c81bc23d13990
IJE2704 <mh1669101@gmail.com>
2023-09-18 22:37:18 +0600

    extract cart and product from local storage data and dilling

--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -3,15 +3,15 @@ import Cart from '../Cart/Cart';
 import { useLoaderData, useRouteLoaderData } from 'react-router-dom';
 
 const Orders = () => {
-    const products = useLoaderData();
-    console.log(products);
+    const cart = useLoaderData();
+    // console.log(products);
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h2>order page</h2>
+                <h2>Products : {cart.length}</h2>
             </div>
             <div className='cart-container'>
-                <Cart cart={[]}></Cart>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -1,7 +1,23 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductLoader = async () =>{
     const loaderProdcucts = await fetch('products.json');
     const products = await loaderProdcucts.json();
-    console.log(products);
+
+    const storedCart = getShoppingCart();
+    const saveCart = [];
+
+    for(const id in storedCart)
+    {
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct)
+        {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            saveCart.push(addedProduct);
+        }
+    }
+    return saveCart;
 }
 
 export default cartProductLoader;
