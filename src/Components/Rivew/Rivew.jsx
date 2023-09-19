dd2006690cb74362decb8b9ab8a72434cc65435a
IJE2704 <mh1669101@gmail.com>
2023-09-19 08:35:12 +0600

    review item design complete

--- src/Components/Rivew/Rivew.jsx
+++ src/Components/Rivew/Rivew.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowAltCircleRight, faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
-const Rivew = ({product}) => {
+const Rivew = ({ product }) => {
     // console.log(cart);
-    const {img, name, price, quantity} = product;
+    const { img, name, price, quantity } = product;
     return (
         <div className='rivew-item'>
             <img src={img} alt="" />
-            <div>
+            <div className='rivew-details'>
                 <h3>{name}</h3>
                 <p>Price : <span className='price'>{price}$</span></p>
+                <p>Quantity : <span className='price'>{quantity}</span></p>
             </div>
+            <button className='remove'><FontAwesomeIcon className='icon' icon={faTrashCan} /></button>
         </div>
     );
 };
