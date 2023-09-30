ecec24d8067cf6c541a82f0fe91f205d04033dc7
IJE2704 <mh1669101@gmail.com>
2023-09-30 21:57:59 +0600

    create sign up page and log in page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './Login.css'
+import { Link } from 'react-router-dom';
+import google from '../../images/google.svg'
 
 const Login = () => {
     return (
-        <div>
-            <h2>Login page</h2>
+        <div className='login-div'>
+            <h2 className='login-text'>Login</h2>
+            <form className='form-container'>
+                <div>
+                <label className='label' htmlFor="email">Email</label>
+                <input className='input-field' type="mail" name='email' placeholder='Email' />
+                </div>
+                <div>
+                <label className='label' htmlFor="password">Password</label>
+                <input className='input-field' type="mail" name='password' placeholder='password' />
+                </div>
+                <button className='btn' type='submit'>Login</button>
+            </form>
+            <p className='transfer'>New to Ema-John? <Link to='/signup'>Create New Account</Link></p>
+            <div className='underline'>
+                <div className='line'></div>
+                <p>or</p>
+                <div className='line'></div>
+            </div>
+            <div>
+                <button className='btn-2'>
+                    <div className='continue-google'>
+                        <img src={google} alt="" />
+                        <p>Continue With Google</p>
+                    </div>
+                </button>
+            </div>
         </div>
     );
 };
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import './SignUp.css'
+import { Link } from 'react-router-dom';
+import google from '../../images/google.svg'
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>sign Up</h1>
+        <div className='signup-div'>
+            <h2 className='login-text'>Sign Up</h2>
+            <form className='form-container'>
+                <div>
+                    <label className='label' htmlFor="email">Email</label>
+                    <input className='input-field' type="mail" name='email' placeholder='Email' />
+                </div>
+                <div>
+                    <label className='label' htmlFor="password">Password</label>
+                    <input className='input-field' type="password" name='password' placeholder='password' />
+                </div>
+                <div>
+                    <label className='label' htmlFor="password">Confirm Password</label>
+                    <input className='input-field' type="password" name='confirm' placeholder='password' />
+                </div>
+                <button className='btn' type='submit'>Sign Up</button>
+            </form>
+            <p className='transfer'>Already have an account? <Link to='/login'>Login</Link></p>
+            <div className='underline'>
+                <div className='line'></div>
+                <p>or</p>
+                <div className='line'></div>
+            </div>
+            <div>
+                <button className='btn-2'>
+                    <div className='continue-google'>
+                        <img src={google} alt="" />
+                        <p>Continue With Google</p>
+                    </div>
+                </button>
+            </div>
         </div>
     );
 };
